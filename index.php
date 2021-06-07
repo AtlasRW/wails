701043da8a10a375691906d3fb43050a4dd6b1fa
atlasrw <atlasrw@icloud.com>
2021-06-07 12:49:33 +0000

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC Framework</title>
+</head>
+<body>
+    <p>SERVER : <? var_dump($_SERVER['REQUEST_URI']) ?></p>
+</body>
+</html>
