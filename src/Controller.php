2d93f615b1dd776eb16f2729e98b4a287349ada9
atlasrw <atlasrw@icloud.com>
2021-06-30 23:46:35 +0000

    Controller

--- src/Controller.php
+++ src/Controller.php
@@ -3,4 +3,15 @@
 namespace Wails\Core;
 
 abstract class Controller
-{}
+{
+
+    protected object $db;
+
+    public final function __construct()
+    {
+
+        // $this->db = new Database();
+
+    }
+
+}
